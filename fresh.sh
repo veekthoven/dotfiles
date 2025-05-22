57c53e2cb859cdf21c29b23fa57f8338bc2e45dc
Victor Abbah Nkoms <veekthoven@gmail.com>
2025-05-22 19:35:53 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,35 +2,51 @@
 
 echo "Setting up your Mac..."
 
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
+# Install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+# Install Homebrew if not already installed
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $HOME/.zprofile
-  eval "$(/usr/local/bin/brew shellenv)"
+  # Set up Homebrew shell environment based on architecture
+  if [[ $(uname -m) = "arm64" ]]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo 'eval "$(/usr/local/bin/brew shellenv)"' >> "$HOME/.zprofile"
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
 fi
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+# Remove existing .zshrc and link to custom one from .dotfiles
+echo "Linking .zshrc from .dotfiles..."
+rm -f "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
 
-# Install zsh plugins: syntax highlighting and zsh autosuggestions.
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Install Zsh plugins
+echo "Installing zsh plugins..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-# Update Homebrew recipes
+git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
+# Update Homebrew
+echo "Updating Homebrew..."
 brew update
 
-$DOTFILES/brewfile.sh
+# Run custom brew install script
+DOTFILES="$HOME/.dotfiles"
+echo "Installing apps and tools from brewfile.sh..."
+sh "$DOTFILES/brewfile.sh"
 
-# Create a Code directory
-mkdir $HOME/code
-
-mkdir $HOME/herd
+# Create work directories
+mkdir -p "$HOME/code"
+mkdir -p "$HOME/herd"
 
 echo "Done setting up your Mac."
